9a3df8a6bf1aec55a17ecb45c429947523499d08
HANKYU PARK <official03x@gmail.com>
2025-02-27 06:08:40 +0900

    studied at 20250226

---
+++ leetcode/arrays/encode and decode strings.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    string myEncode(vector<string>& strs) {
+        string answer;
+        for (const string& str: strs) {
+            answer += str;
+            answer += "-";
+        }
+        return answer;
+    }
+
+    string bestEncode(vector<string>& strs) {
+        string answer;
+        answer.reserve(accumulate(strs.begin(), strs.end(), 0, [](int sum, const string& str) { return sum + str.size() + 1; }));
+
+        for (const string& str : strs) {
+            answer.append(str);
+            answer.push_back('-');
+        }
+        return answer;
+    }
+
+    vector<string> myDecode(string s) {
+        string str;
+        stringstream ss(s);
+        vector<string> answer;
+
+        while (getline(ss, str, '-')) answer.push_back(str);
+
+        return answer;
+    }
+
+    vector<string> bestDecode(string s) {
+        vector<string> answer;
+        size_t start = 0, end;
+        
+        while ((end = s.find('-', start)) != string::npos) {
+            answer.emplace_back(s.substr(start, end - start));
+            start = end + 1;
+        }
+        return answer;
+    }
+};
